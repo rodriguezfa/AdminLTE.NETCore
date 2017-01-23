9c5ff9c698fa58faf6ff32cf03eb42687ba766af
Francisco Rodriguez <love2_play@ymail.com>
2017-01-22 18:39:00 -0800

    Added PhotoSwipe

---
+++ src/AdminLTE.NETCore/Controllers/PhotoSwipeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AdminLTE.NETCore.Attributes;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AdminLTE.NETCore.Controllers
+{
+    [DisplayOrder(10)]
+    [DisplayImage("")]
+    [TreeViewSettings("")]
+    public class PhotoSwipeController : Controller
+    {
+        [DisplayActionMenu]
+        [DisplayImage("fa fa-photo")]
+        [ScriptAfterPartialView("/js/PhotoSwipe/photoswipe.js")]
+        [TreeView("", "label pull-right bg-red", "3")]
+        public IActionResult PhotoSwipe(bool partial = false)
+        {
+            if (partial)
+                return PartialView();
+            else
+                return View();
+        }
+    }
+}
---
+++ src/AdminLTE.NETCore/wwwroot/js/PhotoSwipe/PhotoSwipe.js
@@ -0,0 +1,40 @@
+﻿$(function () {
+
+
+    var openPhotoSwipe = function () {
+        var pswpElement = document.querySelectorAll('.pswp')[0];
+
+        // build items array
+        var items = [
+            {
+                src: 'https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_b.jpg',
+                w: 964,
+                h: 1024
+            },
+            {
+                src: 'https://farm7.staticflickr.com/6175/6176698785_7dee72237e_b.jpg',
+                w: 1024,
+                h: 683
+            }
+        ];
+
+        // define options (if needed)
+        var options = {
+            // history & focus options are disabled on CodePen        
+            history: false,
+            focus: false,
+
+            showAnimationDuration: 0,
+            hideAnimationDuration: 0
+
+        };
+
+        var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery.init();
+    };
+
+    openPhotoSwipe();
+
+    //document.getElementById('btn').onclick = openPhotoSwipe;
+
+});
